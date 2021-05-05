78939d72f71dc5c8e8138612a71c15a025bf5757
Laura <65158950+lauratro@users.noreply.github.com>
2021-05-05 10:06:22 +0200

    Added isLoading feature

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import CharacterCards from './Cards';
-
+import isLoading from './isLoading';
 
 
 function Home() {
     const [characters, setCharacters] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     
     const [search, setSearch] = useState("");
     let fetchApi = () => {
         fetch("https://rickandmortyapi.com/api/character/").then((response )=> response.json())
             .then((data) => {
+                   setIsLoading(false)
                  setCharacters(data.results);
             
-             
+            
     })
     }
    
    
-useEffect(() => {
-    if (search === "") {
-        fetchApi();
-    }
-    else {
-        setCharacters(characters.filter(character => {
-            return character.name.toLowerCase().includes(search.toLowerCase())
-        }))
-    }
+    useEffect(() => {
+        if (isLoading) {
+         
+            if (search === "") {
+               
+                fetchApi();
+                
+            }
+            else {
+                setCharacters(characters.filter(character => {
+                    return character.name.toLowerCase().includes(search.toLowerCase())
+                }))
+                
+            }
+        }
+            else {
+                <isLoading />
+            }
+    
+  
     
     },[search]
         
