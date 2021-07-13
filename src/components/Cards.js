260184644a80768be7a11a117b522daed834ec66
Laura <65158950+lauratro@users.noreply.github.com>
2021-07-13 11:36:37 +0200

    final version

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,47 +1,51 @@
-import React from 'react';
+import React from "react";
 
-import Row from 'react-bootstrap/Row';
+import Row from "react-bootstrap/Row";
 
-import Card from 'react-bootstrap/Card'
-import CardGroup from 'react-bootstrap/CardGroup'
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
 import ModalCreator from "./Modal";
-import '../styles/Cards.css';
+import "../styles/Cards.css";
 
 function CharacterCards(props) {
-    
-    let dataFetch = props.character;
-       
- 
-    let myStyleImg = {
-        width: 300,
-        height: 300
-    }
-    return (<CardGroup id="CardGroup">
-        <Row className="d-flex">
-           
-        {
-            dataFetch.map(character => {
-                return <Card className="p-3" key={character.id}>
-                < div className="flip-card">
-        <div className="flip-card-inner">
-            <div className="flip-card-front">
-                <img src={character.image} alt="Character picture" style={myStyleImg} />
-            </div>
-            <div className="flip-card-back">
-                       <h1>{character.name}</h1>
-                       <p>{character.status }</p>
-                              <ModalCreator myData={character} />
-            </div>
-        </div>
-                    </div>
-                     
+  let characters = props.character;
+  let searchTerm = props.searchTerm;
+  const filteredCharacters = searchTerm
+    ? characters.filter((character) =>
+        character.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : characters;
+  let myStyleImg = {
+    width: 300,
+    height: 300,
+  };
+  return (
+    <CardGroup id="CardGroup">
+      <Row className="d-flex justify-content-center">
+        {filteredCharacters.map((character) => {
+          return (
+            <Card className="p-3" key={character.id}>
+              <div className="flip-card">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <img
+                      src={character.image}
+                      alt="Character picture"
+                      style={myStyleImg}
+                    />
+                  </div>
+                  <div className="flip-card-back">
+                    <h1>{character.name}</h1>
+                    <p>{character.status}</p>
+                    <ModalCreator myData={character} />
+                  </div>
+                </div>
+              </div>
             </Card>
-           })
-              
-                  
-            }
-                   </Row>
-         </CardGroup>
-   )
+          );
+        })}
+      </Row>
+    </CardGroup>
+  );
 }
 export default CharacterCards;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,92 +1,81 @@
-import React, { useState, useEffect } from 'react';
-import CharacterCards from './Cards';
-import '../styles/Home.css'
-import LoadingWheel from './Loading';
-import SearchBarStyle from '../styles/style.js'
-import Input from './SearchBar'
-
+import React, { useState, useEffect } from "react";
+import CharacterCards from "./Cards";
+import "../styles/Home.css";
+import LoadingWheel from "./Loading";
+import SearchBarStyle from "../styles/style.js";
+import Input from "./SearchBar";
 
 function Home() {
-    const [characters, setCharacters] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [count, setCount] = useState(1);
-    const [search, setSearch] = useState("");
+  const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [count, setCount] = useState(1);
+  const [search, setSearch] = useState("");
 
+  let fetchApi = () => {
+    fetch(`https://rickandmortyapi.com/api/character/?page=${count}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters(data.results);
+        setIsLoading(false);
+      });
+  };
 
+  useEffect(() => {
+    fetchApi();
+  }, [search, count]);
 
+  const onchange = (data) => {
+    setSearch(data);
+  };
 
-    let fetchApi = () => {
-        fetch(`https://rickandmortyapi.com/api/character/?page=${count}`).then((response) => response.json())
-            .then((data) => {
-                setCharacters(data.results);
-                setIsLoading(false)
-            
-            
-            })
-    }
-   
-    
-  
-    useEffect(() => {
-        
- 
-     
-           
-        if (search === "") {
-               
-            fetchApi();
-                
-        }
-        else {
-            setCharacters(characters.filter(character => {
-                return character.name.toLowerCase().includes(search.toLowerCase())
-            }))
-                
-            
-    
-        }
-        
-    }, [search, count]
-   
-      
-    )
- 
-    const onchange = (data) => {
-        setSearch(data);
+  // Create handleIncrement event handler
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
 
-}
+  //Create handleDecrement event handler
+  const handleDecrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
 
-        // Create handleIncrement event handler
-        const handleIncrement = () => {
-            setCount(prevCount => prevCount + 1);
-        };
-
-        //Create handleDecrement event handler
-        const handleDecrement = () => {
-            setCount(prevCount => prevCount - 1);
-        };
-    
-   
-  
-    return (
-        
-        <React.Fragment>
-           {/*  <div style={SearchBarStyle()}>
+  return (
+    <React.Fragment>
+      {/*  <div style={SearchBarStyle()}>
             <input  type="text" placeholder="Search" className="mr-sm-2 my-4 p-2 textSearch" onChange={event=>setSearch(event.target.value)} />
          </div> */}
-            <Input data={search} onchange={(e)=>{onchange(e)}} />
-            {!isLoading ?
-                <React.Fragment>
-        <CharacterCards  character={characters} />
-           <div className="btnCounter-container">
-                        <button className="font-weight-bold" onClick={handleDecrement} disabled = {count  === 1 ?true: false }>{"<"}</button>
-        <h5 className="mx-2">Page nr: {count}</h5>
-                        <button class="font-weight-bold" onClick={handleIncrement} disabled = {count  === 34 ?true: false }>{">"}</button>
-                        </div>
-                    </React.Fragment> :   <div><LoadingWheel /></div>
-} 
-    </React.Fragment>         
-)
-           
+      <Input
+        data={search}
+        onchange={(e) => {
+          onchange(e);
+        }}
+      />
+      {!isLoading ? (
+        <React.Fragment>
+          <CharacterCards character={characters} searchTerm={search} />
+          <div className="btnCounter-container">
+            <button
+              className="font-weight-bold"
+              onClick={handleDecrement}
+              disabled={count === 1 ? true : false}
+            >
+              {"<"}
+            </button>
+            <h5 className="mx-2">Page nr: {count}</h5>
+            <button
+              class="font-weight-bold"
+              onClick={handleIncrement}
+              disabled={count === 34 ? true : false}
+            >
+              {">"}
+            </button>
+          </div>
+        </React.Fragment>
+      ) : (
+        <div>
+          <LoadingWheel />
+        </div>
+      )}
+    </React.Fragment>
+  );
 }
 export default Home;
